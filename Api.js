2c6e231dbdf822fdf3e06345031e46859c199dc4
Ana <anva.dev.fasdatec@gmail.com>
2023-09-27 10:37:11 -0600

    modified api.js -Ana

--- Api.js
+++ Api.js
@@ -1,9 +1,13 @@
 function prueba(){
-    w3.getHttpObject("https://api.adviceslip.com/advice",
-    function(data){
+    fetch('https://api.adviceslip.com/advice')
+    .then(response => response.json())
+    .then(function(data) {
+        let ejemplo = data.results;
         ejemplo=data.slip.advice
         document.getElementById("mensaje").innerHTML='"'+ejemplo+'"';
         id=data.slip.id
         document.getElementById("id").innerHTML='#'+id;
-    });
+    })
+    .catch(err=>console.log(err))
 }
+
